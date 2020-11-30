20b19462303d2ffb098f7e2273bab7b924f76837
walliuswilliam <william.wallius@gmail.com>
2020-11-30 06:55:52 +0000

    first gradient descent test

--- src/gradient_descent.py
+++ src/gradient_descent.py
@@ -3,41 +3,52 @@ import math
 def f(x):
   return x**2+2*x+1
 
-class GradienceDescent:
-  def __init__(self, ):
-    
+class GradientDescent:
+  def __init__(self, f, initial_point):
+    self.point = initial_point
+    self.f = f
+
+  def compute_gradient(self, delta):
+    p = self.point
+    print('self', p+0.5*delta)
+    deriv = (self.f(p+0.5*delta)-self.f(p-0.5*delta))/delta
+    return deriv
+  
+  
+  # def descend(self, alpha, delta, num_steps):
+  #   #
+
+  
 
 
 
 
-def g(x):
-  return (x**2+math.cos(x))/(math.e**math.sin(x))
 
-def f3(x):
-  return 1-x**2
+# def g(x):
+#   return (x**2+math.cos(x))/(math.e**math.sin(x))
 
-def gradient_descent(f,x0,alpha=0.1,delta=0.0001,iterations=2):
-  for iteration in range(iterations):
-    deriv = (f(x0+0.5*delta)-f(x0-0.5*delta))/delta
-    x0 = x0-alpha*deriv
-  return f(x0)
+# def f3(x):
+#   return 1-x**2
 
-print('Assignment 22')
-print('gradience of f:', gradient_descent(f, 0))
-print('gradience of fg:', gradient_descent(g, 0))
+# def gradient_descent(f,x0,alpha=0.1,delta=0.0001,iterations=2):
+#   for iteration in range(iterations):
+#     deriv = (f(x0+0.5*delta)-f(x0-0.5*delta))/delta
+#     x0 = x0-alpha*deriv
+#   return f(x0)
 
-def f2(x,y):
-  return 1+x**2+y**2
 
-def g2(x,y):
-  return 1+x**2+2*x+(y**2)-(9*y)
+# def f2(x,y):
+#   return 1+x**2+y**2
 
-def gradient_descent2(f,initial_point,alpha=0.01,delta=0.000001,num_iterations=10000):
-  x_point = initial_point[0]
-  y_point = initial_point[1]
-  for iteration in range(num_iterations): 
-    deriv_x = (f(x_point+0.5*delta, y_point)-f(x_point-0.5*delta, y_point))/delta
-    deriv_y = (f(x_point, y_point+0.5*delta)-f(x_point, y_point-0.5*delta))/delta
-    x_point = x_point-alpha*deriv_x
-    y_point = y_point-alpha*deriv_y
-  return f(x_point, y_point)
+# def g2(x,y):
+#   return 1+x**2+2*x+(y**2)-(9*y)
+
+# def gradient_descent2(f,initial_point,alpha=0.01,delta=0.000001,num_iterations=10000):
+#   x_point = initial_point[0]
+#   y_point = initial_point[1]
+#   for iteration in range(num_iterations): 
+#     deriv_x = (f(x_point+0.5*delta, y_point)-f(x_point-0.5*delta, y_point))/delta
+#     deriv_y = (f(x_point, y_point+0.5*delta)-f(x_point, y_point-0.5*delta))/delta
+#     x_point = x_point-alpha*deriv_x
+#     y_point = y_point-alpha*deriv_y
+#   return f(x_point, y_point)
--- tests/test_gradient_descent.py
+++ tests/test_gradient_descent.py
@@ -1,9 +1,30 @@
+import sys
+sys.path.append('src')
+from gradient_descent import GradientDescent
+
+def single_variable_function(x):
+  return (x-1)**2
+def two_variable_function(x, y):
+  return (x-1)**2 + (y-1)**3
+def three_variable_function(x, y, z):
+  return (x-1)**2 + (y-1)**3 + (z-1)**4
+def six_variable_function(x1, x2, x3, x4, x5, x6):
+  return (x1-1)**2 + (x2-1)**3 + (x3-1)**4 + x4 + 2*x5 + 3*x6
+
+
 minimizer = GradientDescent(f=single_variable_function, initial_point=[0])
+print('testing attribute "point"...')
 assert minimizer.point == [0]
+print('passed')
+
+print('testing method "compute_gradient"...')
 ans = minimizer.compute_gradient(delta=0.01)
-[-2.000] # rounded to 5 decimal places
+assert round(ans, 5) == [-2.000] # rounded to 5 decimal places
+print('passed')
+
 minimizer.descend(alpha=0.001, delta=0.01, num_steps=1)
 assert minimizer.point == [0.002]
+
 
 minimizer = GradientDescent(f=two_variable_function, initial_point=[0,0])
 minimizer.point == [0,0]
