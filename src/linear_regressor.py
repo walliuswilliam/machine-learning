1d2c7ab9685d29910a18cbb937d11e6cf3aed970
walliuswilliam <william.wallius@gmail.com>
2020-12-14 06:19:19 +0000

    first try, converting to dictionary

--- src/linear_regressor.py
+++ src/linear_regressor.py
@@ -10,6 +10,7 @@ class LinearRegressor:
     self.coefficients = self.calculate_coefficients()
   
   def calculate_coefficients(self):
+    final_dict = {}
     mat = Matrix([[1, num] for num in list(self.df.data_dict.values())[0][0]])
     mat_t = mat.transpose()
     mat_mult = mat_t.matrix_multiply(mat)
@@ -17,7 +18,14 @@ class LinearRegressor:
     mat_pseudoinv = mat_inv.matrix_multiply(mat_t)
     multiplier = [[num] for num in list(self.df.data_dict.values())[1][0]]
     multiplier_mat = mat_pseudoinv.matrix_multiply(Matrix(multiplier))
-    return [[round(num, 5) for num in row][0] for row in multiplier_mat.elements]
+    final_dict
+    for num in range(len(multiplier_mat.elements)):
+      if num == 0:
+        key = 'constant'
+      else:
+        key = self.df.data_dict.values[num]
+        final_dict[key] = [row[0] for row in multiplier_mat.elements][num]
+    return final_dict
   
   def predict(self, h_worked):
     return round(self.coefficients[0] + list(h_worked.values())[0]*self.coefficients[1], 5)
