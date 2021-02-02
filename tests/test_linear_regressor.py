603ded8e134d99188de79fc9bd7938e1998e86e2
walliuswilliam <william.wallius@gmail.com>
2021-02-02 03:33:48 +0000

    moved assertion statements for linear and logistic regressor

--- tests/test_linear_regressor.py
+++ tests/test_linear_regressor.py
@@ -37,5 +37,53 @@ assert {k:round(v,8) for k,v in regressor.coefficients.items()} == {'constant': 
 print('passed')
 
 print('testing method "predict"...')
-assert regressor.predict({'scoops of chocolate': 2, 'scoops of vanilla': 3}) == 0.47102804
+assert round(regressor.predict({'scoops of chocolate': 2, 'scoops of vanilla': 3}), 8) == 0.47102804
+
+
+df = DataFrame.from_array(
+    [[0, 0, [],               1],
+    [0, 0, ['mayo'],          1],
+    [0, 0, ['jelly'],         4],
+    [0, 0, ['mayo', 'jelly'], 0],
+    [5, 0, [],                4],
+    [5, 0, ['mayo'],          8],
+    [5, 0, ['jelly'],         1],
+    [5, 0, ['mayo', 'jelly'], 0],
+    [0, 5, [],                5],
+    [0, 5, ['mayo'],          0],
+    [0, 5, ['jelly'],         9],
+    [0, 5, ['mayo', 'jelly'], 0],
+    [5, 5, [],                0],
+    [5, 5, ['mayo'],          0],
+    [5, 5, ['jelly'],         0],
+    [5, 5, ['mayo', 'jelly'], 0]],
+    columns = ['beef', 'pb', 'condiments', 'rating'])
+
+df = df.create_dummy_variables('condiments')
+
+df = df.create_interaction_terms('beef', 'pb')
+df = df.create_interaction_terms('beef', 'mayo')
+df = df.create_interaction_terms('beef', 'jelly')
+df = df.create_interaction_terms('pb', 'mayo',)
+df = df.create_interaction_terms('pb', 'jelly')
+df = df.create_interaction_terms('mayo', 'jelly')
+
+linear_regressor = LinearRegressor(df, dependent_variable='rating')
+
+
+print('testing observation 1...')
+observation = {'beef': 8, 'mayo': 1}
+assert round(linear_regressor.predict(observation), 2) == 11.34
+print('passed')
+
+print('testing observation 2...')
+# test 4 tbsp of pb + 8 slices of beef + mayo
+observation = {'beef': 8, 'pb': 4, 'mayo': 1}
+assert round(linear_regressor.predict(observation), 2) == 3.62
+print('passed')
+
+print('testing observation 3...')
+# test 8 slices of beef + mayo + jelly
+observation = {'beef': 8, 'mayo': 1, 'jelly': 1}
+assert round(linear_regressor.predict(observation), 2) == 2.79
 print('passed')
--- tests/test_logistic_regressor.py
+++ tests/test_logistic_regressor.py
@@ -20,3 +20,51 @@ print('\ntesting predict...')
 assert round(log_reg.predict({'x': 5}), 3) == 0.777, log_reg.predict({'x': 5})
 print('passed')
 
+
+df = DataFrame.from_array(
+    [[0, 0, [],               1],
+    [0, 0, ['mayo'],          1],
+    [0, 0, ['jelly'],         4],
+    [0, 0, ['mayo', 'jelly'], 0.1],
+    [5, 0, [],                4],
+    [5, 0, ['mayo'],          8],
+    [5, 0, ['jelly'],         1],
+    [5, 0, ['mayo', 'jelly'], 0.1],
+    [0, 5, [],                5],
+    [0, 5, ['mayo'],          0.1],
+    [0, 5, ['jelly'],         9],
+    [0, 5, ['mayo', 'jelly'], 0.1],
+    [5, 5, [],                0.1],
+    [5, 5, ['mayo'],          0.1],
+    [5, 5, ['jelly'],         0.1],
+    [5, 5, ['mayo', 'jelly'], 0.1]],
+    columns = ['beef', 'pb', 'condiments', 'rating'])
+
+df = df.create_dummy_variables('condiments')
+
+df = df.create_interaction_terms('beef', 'pb')
+df = df.create_interaction_terms('beef', 'mayo')
+df = df.create_interaction_terms('beef', 'jelly')
+df = df.create_interaction_terms('pb', 'mayo',)
+df = df.create_interaction_terms('pb', 'jelly')
+df = df.create_interaction_terms('mayo', 'jelly')
+
+logistic_regressor = LogisticRegressor(df, dependent_variable='rating', upper_bound=10)
+
+
+print('testing observation 1...')
+observation = {'beef': 8, 'mayo': 1}
+assert round(logistic_regressor.predict(observation), 2) == 9.72
+print('passed')
+
+print('testing observation 2...')
+# test 4 tbsp of pb + 8 slices of beef + mayo
+observation = {'beef': 8, 'pb': 4, 'mayo': 1}
+assert round(logistic_regressor.predict(observation), 2) == 0.77
+print('passed')
+
+print('testing observation 3...')
+# test 8 slices of beef + mayo + jelly
+observation = {'beef': 8, 'mayo': 1, 'jelly': 1}
+assert round(logistic_regressor.predict(observation), 2) == 0.79
+print('passed')
